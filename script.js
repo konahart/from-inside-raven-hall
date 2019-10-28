e70f90dd7c6df5aae0c68069cb3cdbddc3cad224
Glitch (hello-webpage) <none>
2019-10-28 04:14:45 +0000

    💫🍉 Checkpoint ./style.css:6055746/914 ./script.js:6055746/1099 ./index.html:6055746/69

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+ 
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  
+}
