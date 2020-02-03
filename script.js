2ddaa61ef7a9652d6dd72274467fa9bd99c77072
Kona Goodhart <code@konahart.com>
2020-02-02 15:40:07 -1000

    Get regex working for {a|b|c}

--- script.js
+++ script.js
@@ -7,20 +7,39 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function() {
-  const cards = request.response;
-  var deck = shuffleDeck(cards);
+  var cards = request.response;
+  for (card of cards["prompts"]) {
+    card = resolveCard(card);
+  }
+  var deck = shuffle(cards["prompts"]);
+  console.log(deck.length);
   setupDeck(deck);
 }
 
-function shuffleDeck(cards) {
-  
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 function resolveCard(cardText) {
   // Cards may hae {text a|text b} - choose one of the options and remove the extras
-  let re = /{[^}]+}/gm;
-  cardText.matchAll(re);
+  //console.log(cardText)
+  let matches = cardText.matchAll(/{[^}]+}/gm);
+  for (match of matches) {
+    let options = match.toString().replace(/[{}]/gi, "");
+    options = options.split("|");
+    let choice = options[Math.floor(Math.random() * options.length)].toString();
+    cardText = cardText.replace(match, choice);
+  }
+  console.log(cardText);
+  if (cardText.includes("{")||cardText.includes("}")) {
+    console.warn("Extraneous {}s found! " + cardText);
+  }
   
+  return cardText;
 }
 
 function setupDeck(deck) {
