144fd2489e192a33d4c1665e4cf1e0181b491f9d
Kona Goodhart <code@konahart.com>
2020-02-02 16:27:23 -1000

    Remove {} cards, show deck

--- script.js
+++ script.js
@@ -8,12 +8,19 @@ request.responseType = 'json';
 request.send();
 request.onload = function() {
   var cards = request.response;
+  let resolvedCards = [];
   for (card of cards["prompts"]) {
     card = resolveText(card);
+    if (card != null) {
+      resolvedCards.push(card);
+    }
   }
-  var deck = shuffle(cards["prompts"]);
-  console.log(deck.length);
-  setupDeck(deck);
+  cards["prompts"] = resolvedCards;
+  console.log(cards["prompts"][0]);
+  cards["prompts"] = shuffle(cards["prompts"]);
+  console.log(cards["prompts"][0]);
+  setupDeck(cards);
+  showDeck(cards);
 }
 
 function shuffle(array) {
@@ -24,24 +31,36 @@ function shuffle(array) {
   return array;
 }
 
+function randomFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 function resolveText(text) {
   // Text may contain {text a|text b} - choose one of the options and remove the extras
   let matches = text.matchAll(/{[^}]+}/gm);
   for (match of matches) {
     let options = match.toString().replace(/[{}]/gi, "");
     options = options.split("|");
-    let choice = options[Math.floor(Math.random() * options.length)].toString();
+    let choice = randomFromArray(options).toString();
     text = text.replace(match, choice);
   }
   if (text.includes("{")||text.includes("}")) {
-    console.warn("Extraneous {}s found! " + text);
+    console.warn("Extraneous {}s found! \"" + text + "\"");
+    return null;
   }
-  
   return text;
 }
 
-function setupDeck(deck) {
-  
+function setupDeck(cards) {
+  let promptsPerAct = cards["prompts"].length / cards["rituals"].length;
+  let i = 1;
+  for (let value of Object.values(cards["rituals"])) {
+    let ritualPrompt = "Ritual: " + value;
+    console.log(ritualPrompt);
+    
+    cards["prompts"].splice(i * promptsPerAct, 0, ritualPrompt);
+    ++i;
+  }
 }
 
 // <- -> controls
@@ -52,6 +71,21 @@ function drawCard(n) {
 // Image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
+}
+
+function showDeck(cards) {
+  var slideshow = document.getElementsByClassName("slideshow")[0];
+  for (card of cards["prompts"]) {
+    // create slide
+    const slide = document.createElement('div');
+    slide.className = "mySlides fade"
+    const p = document.createElement('p');
+    p.textContent = card;
+    slide.appendChild(p);
+    
+    // add slide to slideshow
+    slideshow.appendChild(slide);
+  }
 }
 
 function showSlides(n) {
