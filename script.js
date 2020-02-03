37e73d9f7502064d1363df189603e26fac8dae74
Kona Goodhart <code@konahart.com>
2020-02-02 17:40:58 -1000

    Wait for game to load before showing slides

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+async function main() {
+  await loadGame()
+  showSlides(slideIndex)
+}
 
-let requestURL = '/cards.json';
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-  var cards = request.response;
+async function loadGame() {
+  const resp = await fetch('/cards.json')
+  const cards = await resp.json()
+
   let resolvedCards = [];
   for (card of cards["prompts"]) {
     card = resolveText(card);
@@ -19,7 +18,7 @@ request.onload = function() {
   console.log(cards["prompts"][0]);
   cards["prompts"] = shuffle(cards["prompts"]);
   console.log(cards["prompts"][0]);
-  setupDeck(cards);
+  prepareDeck(cards);
   showDeck(cards);
 }
 
@@ -51,16 +50,22 @@ function resolveText(text) {
   return text;
 }
 
-function setupDeck(cards) {
-  let promptsPerAct = cards["prompts"].length / cards["rituals"].length;
+function prepareDeck(cards) {
+  let promptsPerAct = cards["prompts"].length / Object.keys(cards["rituals"]).length;
+  console.log(cards["prompts"].length);
+  console.log(cards["rituals"].length);
+  console.log(promptsPerAct);
   let i = 1;
-  for (let value of Object.values(cards["rituals"])) {
-    let ritualPrompt = "Ritual: " + value;
+  for (ritual of cards["rituals"]) {
+    let ritualPrompt = "Ritual: " + ritual[0];
     console.log(ritualPrompt);
-    
+    console.log(i);
+    console.log(i * promptsPerAct);
     cards["prompts"].splice(i * promptsPerAct, 0, ritualPrompt);
     ++i;
   }
+  // insert final ritual
+  //cards["prompts"].push(ritualPrompt);
 }
 
 // <- -> controls
@@ -91,13 +96,17 @@ function showDeck(cards) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  console.log(slides.length);
  
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      slides[i].style.display = "block";
   }
 
   slides[slideIndex-1].style.display = "block";
   
 }
+
+var slideIndex = 1
+main()
