b37ed181b90386fd62f29bc96a685cae90d72ee9
Laurel Kona Goodhart <--global>
2020-02-03 00:22:37 -1000

    Hide non-current slides

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "block";
+      slides[i].style.display = "none";
   }
 
   slides[slideIndex-1].style.display = "block";
